3b0cee48304987637d89d47a017387875deea77b
abdo-355 <abdoluxor366@gmail.com>
2025-09-07 00:09:47 +0300

    Enhance CLI with new commands and API integration
    - Introduced `map` and `mapb` commands to fetch and display location areas from the Pokémon API.
    - Updated command structure to pass a configuration object for managing API state.
    - Added error handling for HTTP requests and JSON parsing.
    - Enhanced help command to include descriptions for new commands.

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -10,7 +13,11 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	Next, Previous string
 }
 
 func main() {
@@ -27,6 +34,20 @@ func main() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the names of 20 location areas in the Pokemon world.",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the names of the previous 20 location areas in the Pokemon world.",
+			callback:    commandMapb,
+		},
+	}
+
+	cfg := config{
+		"", "",
 	}
 
 	for {
@@ -38,7 +59,7 @@ func main() {
 			ci := cleanInput(t)
 
 			if cmd := cmds[strings.ToLower(ci[0])]; cmd.callback != nil {
-				err := cmd.callback()
+				err := cmd.callback(&cfg)
 				if err != nil {
 					fmt.Println("Error:", err)
 				}
@@ -50,19 +71,96 @@ func main() {
 	}
 }
 
-func commandExit() error {
+func commandExit(c *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println("")
 	fmt.Println("help: Displays a help message")
+	fmt.Println("map: displays the names of 20 location areas in the Pokemon world.")
+	fmt.Println("mapb: displays the names of the previous 20 location areas in the Pokemon world.")
 	fmt.Println("exit: Exit the Pokedex")
+
+	return nil
+}
+
+type apiResponse struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func commandMap(c *config) error {
+	if c.Next == "" {
+		c.Next = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	res, err := http.Get(c.Next)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	r := apiResponse{}
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return err
+	}
+
+	c.Next = r.Next
+	if r.Previous == "" {
+		c.Previous = "https://pokeapi.co/api/v2/location-area/"
+	} else {
+		c.Previous = r.Previous
+	}
+
+	for _, v := range r.Results {
+		fmt.Println(v.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.Previous == "" {
+		c.Previous = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	res, err := http.Get(c.Previous)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	r := apiResponse{}
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return err
+	}
+
+	c.Previous = r.Previous
+	c.Next = r.Next
+
+	for _, v := range r.Results {
+		fmt.Println(v.Name)
+	}
 
 	return nil
 }
